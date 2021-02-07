20cfa7e8212528253a77778d842f664c65f2acf5
leonardosibela <leonardo_sibela@hotmail.com>
2021-02-07 16:18:50 -0300

    Adding a column with border and text

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -2,13 +2,8 @@ package com.example.jetpackcompose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.ScrollableColumn
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
@@ -25,50 +20,68 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            ScrollableColumn(
-                modifier = Modifier.background(
-                    color = Color(0xFFF2F2F2)
-                )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .border(
+                        border = BorderStroke(
+                            width = 1.dp,
+                            color = Color.Black
+                        )
+                    )
             ) {
-                Image(
-                    bitmap = imageFromResource(
-                        res = resources,
-                        resId = R.drawable.happy_meal_small
-                    ),
-                    modifier = Modifier.height(300.dp),
-                    contentScale = ContentScale.Crop
-                )
-
-                Column(
-                    modifier = Modifier.padding(16.dp)
-                ) {
-                    Text(
-                        text = "Happy Meal",
-                        style = TextStyle(
-                            fontSize = TextUnit.Companion.Sp(26)
-                        )
-                    )
-
-                    Spacer(modifier = Modifier.padding(top = 10.dp))
-
-                    Text(
-                        text = "800 calories",
-                        style = TextStyle(
-                            fontSize = TextUnit.Companion.Sp(17)
-                        )
-                    )
-
-                    Spacer(modifier = Modifier.padding(top = 10.dp))
-
-                    Text(
-                        text = "$5.99",
-                        style = TextStyle(
-                            color = Color(0xFF85BB65),
-                            fontSize = TextUnit.Companion.Sp(17)
-                        )
-                    )
-                }
+                Text(text = "Item 1")
             }
         }
+
+//        setContent {
+//            ScrollableColumn(
+//                modifier = Modifier
+//                    .fillMaxSize()
+//                    .background(
+//                        color = Color(0xFFF2F2F2)
+//                    )
+//            ) {
+//                Image(
+//                    bitmap = imageFromResource(
+//                        res = resources,
+//                        resId = R.drawable.happy_meal_small
+//                    ),
+//                    modifier = Modifier.height(300.dp),
+//                    contentScale = ContentScale.Crop
+//                )
+//
+//                Column(
+//                    modifier = Modifier.padding(16.dp)
+//                ) {
+//                    Text(
+//                        text = "Happy Meal",
+//                        style = TextStyle(
+//                            fontSize = TextUnit.Companion.Sp(26)
+//                        )
+//                    )
+//
+//                    Spacer(modifier = Modifier.padding(top = 10.dp))
+//
+//                    Text(
+//                        text = "800 calories",
+//                        style = TextStyle(
+//                            fontSize = TextUnit.Companion.Sp(17)
+//                        )
+//                    )
+//
+//                    Spacer(modifier = Modifier.padding(top = 10.dp))
+//
+//                    Text(
+//                        text = "$5.99",
+//                        style = TextStyle(
+//                            color = Color(0xFF85BB65),
+//                            fontSize = TextUnit.Companion.Sp(17)
+//                        )
+//                    )
+//                }
+//            }
+//        }
     }
 }
