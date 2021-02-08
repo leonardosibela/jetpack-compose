1b651c6ae43050a5af98129efb531f78c0b59d98
leonardosibela <leonardo_sibela@hotmail.com>
2021-02-07 22:34:41 -0300

    Using Row and Coloumn in a real layoyt example

---
+++ app/src/main/java/com/example/jetpackcompose/HappyMealActivity.kt
@@ -0,0 +1,86 @@
+package com.example.jetpackcompose
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.imageFromResource
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+
+class HappyMealActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            ScrollableColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        color = Color(0xFFF2F2F2)
+                    )
+            ) {
+                Image(
+                    bitmap = imageFromResource(
+                        res = resources,
+                        resId = R.drawable.happy_meal_small
+                    ),
+                    modifier = Modifier.height(300.dp),
+                    contentScale = ContentScale.Crop
+                )
+
+                Column(
+                    modifier = Modifier.padding(16.dp)
+                ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        Text(
+                            text = "Happy Meal",
+                            style = TextStyle(
+                                fontSize = TextUnit.Companion.Sp(26)
+                            )
+                        )
+
+                        Text(
+                            text = "$5.99",
+                            style = TextStyle(
+                                color = Color(0xFF85BB65),
+                                fontSize = TextUnit.Sp(17)
+                            ),
+                            modifier = Modifier.align(Alignment.CenterVertically)
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+
+                    Text(
+                        text = "800 calories",
+                        style = TextStyle(
+                            fontSize = TextUnit.Companion.Sp(17)
+                        )
+                    )
+
+                    Spacer(modifier = Modifier.padding(top = 24.dp))
+                    
+                    Button(
+                        onClick = {},
+                        modifier = Modifier.align(Alignment.CenterHorizontally)
+                    ) {
+                        Text(text = "ORDER NOW")
+                    }
+                }
+            }
+        }
+    }
+}
